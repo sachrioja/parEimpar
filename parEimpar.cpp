6c98b083599468edb6b7a2f6d8dec1476b3ce944
Sach Rioja <41712991+sachrioja@users.noreply.github.com>
2026-08-14 01:57:27 -0600

    Add files via upload

---
+++ parEimpar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+   int x; //Declarar variable
+   std::cout<<"Digite un numero: ";
+   std::cin>>x; //Ingresar valor
+
+   if(x%2 == 0){   //Condicionales
+       std::cout<<"El numero es par";
+   }
+   else{
+       std::cout<<"El numero es impar";
+   }
+    
+    getch();
+    return 0;
+}
+
